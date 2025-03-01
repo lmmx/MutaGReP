b3773a049263b9069491db3dfa3e5a04c0b20ea2
Louis Maddox <louismmx@gmail.com>
2025-03-01 00:58:55 +0000

    chore: fork and add metadata

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup  # type: ignore
-
-setup(
-    name="mutagrep",
-    version="0.1",
-    packages=["mutagrep"],
-    package_dir={"": "src"},
-    install_requires=[],
-)
